a74db458c50b482414fb17ad47370c28586b018b
Nat <nat.wrw@gmail.com>
2025-08-30 18:53:59 +0700

    Revert "chore: Include hash comparison in /recv response (no CLI changes) (#58)"
    This reverts commit 191207b9dc9ac008d72dbc5b8c9630891e68fa49.

--- pb_hooks/100_data_import_handler.pb.js
+++ pb_hooks/100_data_import_handler.pb.js
@@ -146,7 +146,6 @@ routerAdd("POST", "/recv", (e) => {
       const resp = buildDuplicateResponse(duplicate)
       resp.computed_hash = computedHash
       if (providedHash) resp.provided_hash = providedHash
-      resp.hash_match = providedHash ? (providedHash === computedHash) : null
       return e.json(200, resp)
     }
 
@@ -159,7 +158,6 @@ routerAdd("POST", "/recv", (e) => {
     const resp = buildSuccessResponse(record, itemCount)
     resp.computed_hash = computedHash
     if (providedHash) resp.provided_hash = providedHash
-    resp.hash_match = providedHash ? (providedHash === computedHash) : null
     return e.json(200, resp)
 
   } catch (error) {
